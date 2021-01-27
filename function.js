77dd2ec1205b6eb6952e6c943a2e2e05f90f1b2b
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-01-27 19:25:34 +0600

    array er modde shobgula result

--- function.js
+++ function.js
@@ -1,34 +1,43 @@
-// function eveniffy(num){
-//     if(num % 2 == 0){
-//         console.log(num, ': is even number')
-//     }
-//     else{
-//         console.log(num*2, '" is odd number')
-//     }
-// }
+function evenify(num){
+    var result;
+    if(num % 2 == 0){
+        //console.log(num, ': is even number')
+        result = num;
+    }
+    else{
+        //console.log(num*2, ' : is odd number')
+        result = num *2;
+    }
+   return result;
+}
+// var result = evenify(13);
+// console.log('result :',result);
+var result = evenify(13);
+var square = result * result;
+console.log('square :',square);
+
 function evenify_all(nums){
+    even_array = []
     for (let i = 0; i< nums.length; i++) {
         const num = nums[i];
-        if(num % 2 == 0){
-            console.log(num, ': is even number')
-        }
-        else{
-            console.log(num*2, '" is odd number')
-        }
-    
+        var result = evenify(num)
+        even_array.push(result);
     }
+    return even_array;
 }
 
 nums = [5, 12, 89, 45, 18, 8]
-evenify_all(nums)
+var nums_even = evenify_all(nums)
+console.log(nums_even)
+//evenify_all(nums)
 
 // for (let i = 0; i< nums.length; i++) {
 //         const num = nums[i];
 //      eveniffy(num)
 //     }
 
-    friends_age = [13, 17, 19, 20, 18];
-    evenify_all(nums)
+    // friends_age = [13, 17, 19, 20, 18];
+    // evenify_all(nums)
     // for (let i = 0; i < friends_age.length; i++) {
     //     const age = friends_age[i];
     //     // console.log(age);
